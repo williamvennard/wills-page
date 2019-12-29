b6896520ab05e3a5ca6d8bd665897092958ed61a
williamvennard <wvennard@gradientone.com>
2019-12-28 17:38:21 -0800

    Updates

---
+++ precache-manifest.9d15ab894f465a38e6f557d08725df64.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16a7bccbfdf056000aa9689901832ac3",
+    "url": "/williamvennard/wills-page/index.html"
+  },
+  {
+    "revision": "7c260300870695ab193f",
+    "url": "/williamvennard/wills-page/static/css/2.a9858c30.chunk.css"
+  },
+  {
+    "revision": "f25066a2ebf8bc8a691c",
+    "url": "/williamvennard/wills-page/static/css/main.f0f76bc2.chunk.css"
+  },
+  {
+    "revision": "7c260300870695ab193f",
+    "url": "/williamvennard/wills-page/static/js/2.0b4e5190.chunk.js"
+  },
+  {
+    "revision": "c2ca14468f78f908c37847bb4ba35cc2",
+    "url": "/williamvennard/wills-page/static/js/2.0b4e5190.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f25066a2ebf8bc8a691c",
+    "url": "/williamvennard/wills-page/static/js/main.d7993cfe.chunk.js"
+  },
+  {
+    "revision": "502c64ce5d3c02bf2c0f",
+    "url": "/williamvennard/wills-page/static/js/runtime-main.cb7ccf36.js"
+  },
+  {
+    "revision": "1025a6e0fb0fa86f17f57cc82a6b9756",
+    "url": "/williamvennard/wills-page/static/media/OpenSans-Bold.1025a6e0.ttf"
+  },
+  {
+    "revision": "2d0bdc8df10dee036ca3bedf6f3647c6",
+    "url": "/williamvennard/wills-page/static/media/OpenSans-Light.2d0bdc8d.ttf"
+  },
+  {
+    "revision": "c25bfd42c6d2c7dc089ec3c2fcd67657",
+    "url": "/williamvennard/wills-page/static/media/mountainsPurple.c25bfd42.jpg"
+  }
+]);
