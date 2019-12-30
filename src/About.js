9f55fce108e972cc7f12f8c84b3348059b071146
williamvennard <wvennard@gradientone.com>
2019-12-29 23:37:51 -0800

    Change title.

--- src/About.js
+++ src/About.js
@@ -13,33 +13,12 @@ function About(props) {
 	      I have been doing Web Development ever since. 
 	    </p> 
 	    <p>
-	    	I am currently a Front-End Web Developer at GradentOne, where for the past few years, I've been 
+	    	I am currently a Web Developer at GradentOne, where for the past few years, I've been 
 	      lucky enough to help build their product from the ground up. GradientOne's is a test automation 
 	      cloud platform with a web browser interface and a robust API for those who like to dig deeper. 
 	      GradientOne enables test engineers to do asset utilization tracking, hardware test automation, 
-	      test data acquisition and archiving, as well as advanced data analytics.
+	      test data acquisition and archiving and much more. Click the link below to learn more. 
 	    </p>
-
-	    <p>
-	    	Alii dicant hendrerit mei an, per iudicabit scriptorem ne. Aeque conceptam deterruisset in qui. 
-	    	Minim sadipscing appellantur in his, idque dicunt ponderum ad usu. Te vocent aliquip alterum sea, 
-	    	ex delectus phaedrum ius. Congue placerat repudiare et sit. Blandit mentitum atomorum per cu. Ad 
-	    	pro noluisse dissentiunt, ius error tempor ut.
-	    </p>
-
-	    <p>
-	    	Alii dicant hendrerit mei an, per iudicabit scriptorem ne. Aeque conceptam deterruisset in qui. 
-	    	Minim sadipscing appellantur in his, idque dicunt ponderum ad usu. Te vocent aliquip alterum sea, 
-	    	ex delectus phaedrum ius. Congue placerat repudiare et sit. Blandit mentitum atomorum per cu. Ad 
-	    	pro noluisse dissentiunt, ius error tempor ut.
-	    </p>
-	    <p>
-	    	Alii dicant hendrerit mei an, per iudicabit scriptorem ne. Aeque conceptam deterruisset in qui. 
-	    	Minim sadipscing appellantur in his, idque dicunt ponderum ad usu. Te vocent aliquip alterum sea, 
-	    	ex delectus phaedrum ius. Congue placerat repudiare et sit. Blandit mentitum atomorum per cu. Ad 
-	    	pro noluisse dissentiunt, ius error tempor ut.
-	    </p>
-
 	    <a href="https://www.gradientone.com/">Go to GradientOne.com</a>
 
     </div>
