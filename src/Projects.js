b0924623abfe90d54ca37ed86f6387ca2c566e95
williamvennard <wvennard@gradientone.com>
2019-12-29 23:31:27 -0800

    Adds d3plot thumbnail and fixes a link issue.

--- src/Projects.js
+++ src/Projects.js
@@ -1,20 +1,21 @@
 import React from "react"
 import Thumbnail from './Thumbnail.js'; // Import the Thumbnail component
 import './App.css';
-import snakeThumbnail from './snakeProject.png'
+import snakeThumbnail from './snakeProject.png';
+import d3PlotThumbnail from './GradientOnePlot.png';
 
 function Projects(props) {
   return (
     <div className="container-fluid project-container">
     	<h1>Projects</h1>
-    	<p>Look at all these cool projects!</p>
     	<div className="row">
 		    <div className="col-sm">
 		      <Thumbnail
-			  		link="/d3plot"
-			  		image="http://via.placeholder.com/400x360"
+			  		link="https://examples.gradientone.com/results/R4573773855883741"
+			  		image={d3PlotThumbnail}
 			  		title="Interactive D3 Plot"
 			  		category="Data Visualization"
+			  		externalLink = "true"
 					/>
 		    </div>
 		    <div className="col-sm">
@@ -23,6 +24,7 @@ function Projects(props) {
 			  		image={snakeThumbnail}
 			  		title="Play Snake"
 			  		category="Mini Game using D3"
+			  		externalLink = "false"
 					/>
 		    </div>
 		  </div>
--- src/Thumbnail.js
+++ src/Thumbnail.js
@@ -2,18 +2,44 @@ import React from 'react'; // Import the Component component from React
 import {Link} from 'react-router-dom'; // Import the Link component
 import './App.css';
  
-function Thumbnail(props) {
-  return (
-    <div className="project">
-      <Link to={props.link}>
+class Content extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
         <div className="project-image">
-          <img src={props.image} alt="Project Image"/>
+          <img src={this.props.props.image} alt="Project Image"/>
         </div>
-        <div className="project-title">{props.title}</div>
-        <div className="project-category">{props.category}</div>
-      </Link>
-    </div>
-  );
+        <div className="project-title">{this.props.props.title}</div>
+        <div className="project-category">{this.props.props.category}</div>
+      </div>
+    );
+  }
+}
+
+function Thumbnail(props) {
+  // use a tag if using extenal link
+  if(props.externalLink === "true") {
+    return (
+      <div className="project">
+        <a href={props.link}>
+          <Content props={props}/>
+        </a>
+      </div>
+    );
+  } else {
+    return (
+      <div className="project">
+        <Link to={props.link}>
+          <Content props={props}/>
+        </Link>
+      </div>
+    );
+  }
+  
 }
  
 export default Thumbnail;
