03f8c1dda7298d316d56d2a092e7b6261a2767b7
williamvennard <wvennard@gradientone.com>
2020-01-02 14:41:30 -0800

    Using google-maps-react now, have map and current location working.

--- src/Planit.js
+++ src/Planit.js
@@ -1,54 +1,60 @@
-// import React from "react"
-// import './App.css';
- 
-// // Google maps api key AIzaSyAQnnrSxmjA4pPJuFsiSUBjZ0gL0Uq9D7Q
-
-
-// function Planit(props) {
-//   return (
-//     <div className="container-fluid about-container">
-//       <h1>Planit</h1>
-//       <p>This project is in development</p>
-//       <div id="map"></div>
-	    
-//     </div>
-//   )
-// }
- 
-// export default Planit;
-
 import React, { Component } from 'react';
-import GoogleMapReact from 'google-map-react';
- 
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
- 
-class SimpleMap extends Component {
-  static defaultProps = {
-    center: {
-      lat: 59.95,
-      lng: 30.33
-    },
-    zoom: 11
+import './App.css';
+// Google maps api key AIzaSyAQnnrSxmjA4pPJuFsiSUBjZ0gL0Uq9D7Q
+import { Map, GoogleApiWrapper, InfoWindow, Marker } from 'google-maps-react';
+
+
+import CurrentLocation from './Map';
+
+const mapStyles = {
+  width: '100%',
+  height: '100%'
+};
+
+export class MapContainer extends Component {
+  state = {
+    showingInfoWindow: false,
+    activeMarker: {},
+    selectedPlace: {}
   };
- 
+
+  onMarkerClick = (props, marker, e) =>
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+    });
+
+  onClose = props => {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      });
+    }
+  };
+
   render() {
     return (
-      // Important! Always set the container height explicitly
-      <div style={{ height: '100vh', width: '100%' }}>
-        <GoogleMapReact
-          bootstrapURLKeys={{ key: 'AIzaSyAQnnrSxmjA4pPJuFsiSUBjZ0gL0Uq9D7Q' }}
-          defaultCenter={this.props.center}
-          defaultZoom={this.props.zoom}
+      <CurrentLocation
+        centerAroundCurrentLocation
+        google={this.props.google}
+      >
+        <Marker onClick={this.onMarkerClick} name={'current location'} />
+        <InfoWindow
+          marker={this.state.activeMarker}
+          visible={this.state.showingInfoWindow}
+          onClose={this.onClose}
         >
-          <AnyReactComponent
-            lat={59.955413}
-            lng={30.337844}
-            text="Hello World"
-          />
-        </GoogleMapReact>
-      </div>
+          <div>
+            <h4>{this.state.selectedPlace.name}</h4>
+          </div>
+        </InfoWindow>
+      </CurrentLocation>
     );
   }
 }
- 
-export default SimpleMap;
+
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyAQnnrSxmjA4pPJuFsiSUBjZ0gL0Uq9D7Q'
+})(MapContainer);
